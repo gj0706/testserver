e01c434819a316709676e68b3673499012cf2fe4
gj0706 <guojian0706@gmail.com>
2024-11-05 21:49:51 -0600

    Added static file hosting

--- app.js
+++ app.js
@@ -31,11 +31,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use("/auth", auth);
 // app.use("/", indexRouter);
 // app.use("/users", usersRouter);
-
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // get all users
 app.get("/getUsers", async (_, res) => {
 	try {
